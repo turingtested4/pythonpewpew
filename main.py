945a054c85942ec55786d24d0b6d82495a681594
md <mariadolorespersonal@gmail.com>
2026-01-20 08:02:06 +0100

    primer juego en python

---
+++ main.py
@@ -0,0 +1,24 @@
+import pygame
+from constants import SCREEN_HEIGHT,SCREEN_WIDTH
+from logger import log_state
+
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+
+def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while (True):
+        log_state()
+        screen.fill("black")
+        for event in pygame.event.get():
+             if event.type == pygame.QUIT:
+                return False
+        
+
+
+if __name__ == "__main__":
+    main()
